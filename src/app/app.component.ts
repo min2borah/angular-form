61df1e91f9e18db12ad93a95274225448803d480
min2borah <mintu@oari.co>
2022-02-18 17:20:05 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,106 @@
+import { Component, VERSION } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'Input',
+      type: 'input',
+      templateOptions: {
+        label: 'Input',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+      },
+    },
+    {
+      key: 'Textarea',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Textarea',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+        rows: 3,
+      },
+    },
+
+    {
+      key: 'Checkbox',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Accept terms',
+        description: 'In order to proceed, please accept terms',
+        pattern: 'true',
+        required: true,
+      },
+      validation: {
+        messages: {
+          pattern: 'Please accept the terms',
+        },
+      },
+    },
+
+    {
+      key: 'Radio',
+      type: 'radio',
+      templateOptions: {
+        label: 'Radio',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+        options: [
+          { value: 1, label: 'Option 1' },
+          { value: 2, label: 'Option 2' },
+          { value: 3, label: 'Option 3' },
+          { value: 4, label: 'Option 4', disabled: true },
+        ],
+      },
+    },
+
+    {
+      key: 'Select',
+      type: 'select',
+      templateOptions: {
+        label: 'Select',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+        options: [
+          { value: 1, label: 'Option 1' },
+          { value: 2, label: 'Option 2' },
+          { value: 3, label: 'Option 3' },
+          { value: 4, label: 'Option 4', disabled: true },
+        ],
+      },
+    },
+
+    {
+      key: 'select_multi',
+      type: 'select',
+      templateOptions: {
+        label: 'Select Multiple',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+        multiple: true,
+        selectAllOption: 'Select All',
+        options: [
+          { value: 1, label: 'Option 1' },
+          { value: 2, label: 'Option 2' },
+          { value: 3, label: 'Option 3' },
+          { value: 4, label: 'Option 4', disabled: true },
+        ],
+      },
+    },
+  ];
+}
---
+++ src/app/components/slider-component/slider-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-component',
+  templateUrl: './slider-component.component.html',
+  styleUrls: ['./slider-component.component.css'],
+})
+export class SliderComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
